588e35b6c06b8523f097a322387c340056b6b953
Andras Bacsai <andras.bacsai@gmail.com>
2022-02-07 16:57:12 +0100

    Revert "Revert "feat: Generate subsets for all fonts""
    This reverts commit 159ab0a9908593b8d86a56c34c6bb710f0e3279b.

--- gather-fonts.mjs
+++ gather-fonts.mjs
@@ -2,15 +2,50 @@ import got from 'got';
 import { promises as fs } from 'fs';
 import { promisify } from 'node:util';
 import stream from 'node:stream';
-import 'dotenv/config'
+import css from 'css';
+
+import 'dotenv/config';
 
 const data = await got.get(`https://google-webfonts-helper.herokuapp.com/api/fonts/`).json()
 
-const pipeline = promisify(stream.pipeline);
-
-const families = data.map(d => d.id).filter(n => n)
 const storageZoneName = 'coolfonts'
+const families = data.map(d => d.id).filter(n => n)
 
+const pipeline = promisify(stream.pipeline);
+const subsets = new Set()
+for (const subset of data) {
+    for (const range of subset.subsets) {
+        subsets.add(range)
+    }
+}
+
+// Generate subsets
+const allSubsets = {}
+let currentSubset;
+for (const subset of Array.from(subsets)) {
+    console.log('Checking subset: ', subset)
+    const example = data.find(d => d.subsets.includes(subset))
+    const gf = await got.get(`https://fonts.googleapis.com/css2?family=${example.family}`, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+        }
+    }).text()
+    const parsedCss = css.parse(gf)
+
+    parsedCss.stylesheet.rules.map(a => {
+        if (a.type === 'comment') {
+            currentSubset = a.comment.trim()
+            allSubsets[a.comment.trim()] = ''
+        }
+        if (a.type === 'font-face') {
+            const value = a.declarations.find(d => d.property === 'unicode-range').value
+            allSubsets[currentSubset] = value
+        }
+    })
+}
+await fs.writeFile('./subsets.json', JSON.stringify(allSubsets))
+
+// Upload fonts to BunnyCND
 for (const family of families) {
     console.log('Doing: ', family)
     const { variants } = await got.get(`https://google-webfonts-helper.herokuapp.com/api/fonts/${family}`).json();
