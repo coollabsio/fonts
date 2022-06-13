c3f2e41481ac9fdbc1ab4991d4f39720137b1fc8
JUB0T <jforeverything2007@gmail.com>
2022-06-13 09:06:08 +0200

    :white_check_mark: Typescript

--- gather-fonts.ts
+++ gather-fonts.ts
@@ -1,82 +1,91 @@
-import got from "got";
 import css from "css";
+const got = require("got");
 import stream from "node:stream";
 import { promises as fs } from "fs";
 import { promisify } from "node:util";
 
 import "dotenv/config";
 
-const data = await got
-  .get(`https://google-webfonts-helper.herokuapp.com/api/fonts/`)
-  .json();
-
-const storageZoneName = "coolfonts";
-const families = data.map((d) => d.id).filter((n) => n);
-
-const pipeline = promisify(stream.pipeline);
-const subsets = new Set();
-for (const subset of data) {
-  for (const range of subset.subsets) {
-    subsets.add(range);
-  }
-}
-
-// Generate subsets
-let currentSubset;
-const allSubsets = {};
-
-for (const subset of Array.from(subsets)) {
-  console.log("Checking subset: ", subset);
-  const example = data.find((d) => d.subsets.includes(subset));
-  const gf = await got
-    .get(`https://fonts.googleapis.com/css2?family=${example.family}`, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
-      },
-    })
-    .text();
-  const parsedCss = css.parse(gf);
-
-  parsedCss.stylesheet.rules.map((a) => {
-    if (a.type === "comment") {
-      currentSubset = a.comment.trim();
-      allSubsets[a.comment.trim()] = "";
-    }
-    if (a.type === "font-face") {
-      const value = a.declarations.find(
-        (d) => d.property === "unicode-range"
-      ).value;
-      allSubsets[currentSubset] = value;
-    }
-  });
-}
-await fs.writeFile("./subsets.json", JSON.stringify(allSubsets));
-
-// Upload fonts to BunnyCND
-for (const family of families) {
-  console.log("Doing: ", family);
-  const { variants } = await got
-    .get(`https://google-webfonts-helper.herokuapp.com/api/fonts/${family}`)
+async function main() {
+  var data: any[] = await got
+    .get(`https://google-webfonts-helper.herokuapp.com/api/fonts/`)
     .json();
-  for (const variant of variants) {
-    const id = variant.fontWeight;
-    const dir = `./${family}/${variant.fontStyle}/`;
-    try {
-      await pipeline(
-        got.stream(`${variant.woff2}`),
-        await got.stream.put(
-          `https://storage.bunnycdn.com/${storageZoneName}/${dir}/${id}.woff2`,
-          {
-            headers: {
-              AccessKey: process.env.BUNNYCDN_ACCESS_KEY,
-            },
-          }
-        ),
-        new stream.PassThrough()
-      );
-    } catch (err) {
-      console.log(err);
+  const storageZoneName = "coolfonts";
+  const families = data.map((d) => d.id).filter((n) => n);
+
+  const pipeline = promisify(stream.pipeline);
+  const subsets = new Set();
+  for (const subset of data) {
+    for (const range of subset.subsets) {
+      subsets.add(range);
+    }
+  }
+
+  // Generate subsets
+  const allSubsets: any = {};
+
+  for (const subset of Array.from(subsets)) {
+    console.log("Checking subset: ", subset);
+
+    const example = data.find((d) => d.subsets.includes(subset));
+    const gf = await got
+      .get(`https://fonts.googleapis.com/css2?family=${example.family}`, {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+        },
+      })
+      .text();
+
+    const parsedCss = css.parse(gf);
+    parsedCss.stylesheet.rules.map(
+      (a: {
+        comment: any;
+        type: string;
+        declarations: { property: string; value: any }[];
+      }) => {
+        if (a.type === "comment") {
+          var currentSubset = a.comment.trim();
+          allSubsets[a.comment.trim()] = "";
+        }
+        if (a.type === "font-face") {
+          const value = a.declarations.find(
+            (d) => d.property === "unicode-range"
+          ).value;
+          allSubsets[currentSubset] = value;
+        }
+      }
+    );
+  }
+
+  await fs.writeFile("./subsets.json", JSON.stringify(allSubsets));
+  for (const family of families) {
+    console.log("Doing: ", family);
+    const { variants }: any = await got
+      .get(`https://google-webfonts-helper.herokuapp.com/api/fonts/${family}`)
+      .json();
+
+    for (const variant of variants) {
+      const id = variant.fontWeight;
+      const dir = `./${family}/${variant.fontStyle}/`;
+
+      try {
+        await pipeline(
+          got.stream(`${variant.woff2}`),
+          await got.stream.put(
+            `https://storage.bunnycdn.com/${storageZoneName}/${dir}/${id}.woff2`,
+            {
+              headers: {
+                AccessKey: process.env.BUNNYCDN_ACCESS_KEY,
+              },
+            }
+          ),
+          new stream.PassThrough()
+        );
+      } catch (err) {
+        console.log(err);
+      }
     }
   }
 }
+main();
--- index.ts
+++ index.ts
@@ -1,25 +1,26 @@
 import "dotenv/config";
-
-import got from "got";
 import fs from "fs/promises";
-import fastify from "fastify";
 import etag from "fastify-etag";
 
-const server = fastify();
-const domain = process.env.DOMAIN;
+const got = require("got");
+const fastify = require("fastify");
 
-const data = await got
-  .get(`https://google-webfonts-helper.herokuapp.com/api/fonts/`)
-  .json();
-const subsets = JSON.parse(await fs.readFile("./subsets.json", "utf8"));
+async function main() {
+  const server = fastify();
+  const domain = process.env.DOMAIN;
+  const subsets = JSON.parse(await fs.readFile("./subsets.json", "utf8"));
+  const data: { id: string | number; properties?: object; subsets: any }[] =
+    await got
+      .get(`https://google-webfonts-helper.herokuapp.com/api/fonts/`)
+      .json();
 
-server.register(etag);
-server.get("/", (response, reply) => {
-  reply.redirect("https://fonts.coollabs.io");
-});
+  server.register(etag);
+  server.get("/", (req: any, res: any) => {
+    res.redirect("https://fonts.coollabs.io");
+  });
 
-server.get("/icon", async (request, reply) => {
-  const css = `/* fallback */
+  server.get("/icon", async (req: any, res: any) => {
+    const css = `/* fallback */
 @font-face {
   font-family: 'Material Icons';
   font-style: normal;
@@ -42,73 +43,75 @@ server.get("/icon", async (request, reply) => {
   -webkit-font-feature-settings: 'liga';
   -webkit-font-smoothing: antialiased;
 }`;
-  reply.header("content-type", "text/css");
-  reply.send(css);
-  return reply.end();
-});
-server.get("/css2", async (request: any, reply: any) => {
-  try {
-    let { family: families, display } = request.query;
-    if (families) {
-      if (typeof families === "string") {
-        families = [families];
-      }
-      const payload = [];
-      for (let family of families) {
-        let style = "normal";
-        let weights = ["400"];
+    res.header("content-type", "text/css");
+    res.send(css);
+    return res.end();
+  });
 
-        let dashFamily = family.toLowerCase().replace(/ /g, "-");
-        if (family.includes("wght") || family.includes("ital")) {
-          weights = family
-            .split(":")[1]
-            .split("@")[1]
-            .split(";")
-            .filter((n) => n);
-          dashFamily = family.toLowerCase().replace(/ /g, "-").split(":")[0];
+  server.get("/css2", async (request: any, reply: any) => {
+    try {
+      let { family: families, display } = request.query;
+      if (families) {
+        if (typeof families === "string") {
+          families = [families];
         }
+        const payload = [];
+        for (let family of families) {
+          let style = "normal";
+          let weights = ["400"];
 
-        family = family.split(":")[0];
-        const properties = data.find((f) => f.id === dashFamily);
-        for (let weight of weights) {
-          for (const subset of properties?.subsets) {
-            if (weight.includes(",")) {
-              style = weight.split(",")[0] === "0" ? "normal" : "italic";
-            }
-            weight = weight.includes(",") ? weight.split(",")[1] : weight;
-            let css = `
+          let dashFamily = family.toLowerCase().replace(/ /g, "-");
+          if (family.includes("wght") || family.includes("ital")) {
+            weights = family
+              .split(":")[1]
+              .split("@")[1]
+              .split(";")
+              .filter((n: any) => n);
+            dashFamily = family.toLowerCase().replace(/ /g, "-").split(":")[0];
+          }
+
+          family = family.split(":")[0];
+          const properties = data.find((f) => f.id === dashFamily);
+          for (let weight of weights) {
+            for (const subset of properties?.subsets) {
+              if (weight.includes(",")) {
+                style = weight.split(",")[0] === "0" ? "normal" : "italic";
+              }
+              weight = weight.includes(",") ? weight.split(",")[1] : weight;
+              let css = `
   /* ${subset} */
   @font-face {
     font-family: '${family}';
     font-style: ${style};
     font-weight: ${weight};`;
-            if (display)
-              css += `
+              if (display)
+                css += `
     font-display: swap;`;
-            css += `
+              css += `
     src: url(https://${domain}/${dashFamily}/${style}/${weight}.woff2) format('woff2');
     unicode-range: ${subsets[subset]};
   }`;
-            payload.push(css);
+              payload.push(css);
+            }
           }
         }
+        reply.header("content-type", "text/css");
+        return reply.send(payload.join(" ").trim());
+      } else {
+        throw { statusCode: 500, message: "Wrong request" };
       }
-      reply.header("content-type", "text/css");
-      return reply.send(payload.join(" ").trim());
-    } else {
-      throw { statusCode: 500, message: "Wrong request" };
+    } catch (error) {
+      throw { statusCode: 500, message: error.message };
     }
-  } catch (error) {
-    throw { statusCode: 500, message: error.message };
-  }
-});
+  });
 
-try {
-  await server.listen(3000, "127.0.0.1");
-  console.log(
-    `Server listening on http://127.0.0.1:${server.server.address().port}`
-  );
-} catch (err) {
-  server.log.error(err);
-  process.exit(1);
+  try {
+    await server.listen(process.env.PORT);
+    console.log(`Server listening on http://127.0.0.1:${process.env.PORT}`);
+  } catch (err) {
+    console.log(err);
+    server.log.error(err);
+    process.exit(1);
+  }
 }
+main();
